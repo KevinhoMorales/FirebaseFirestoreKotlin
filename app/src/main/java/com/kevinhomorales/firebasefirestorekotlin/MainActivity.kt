4d4690bde2a2463bea6e37aefd0574c2de823b05
Kevin Morales <kfmorales94@gmail.com>
2024-06-15 12:34:23 -0500

    Project init

---
+++ app/src/main/java/com/kevinhomorales/firebasefirestorekotlin/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.kevinhomorales.firebasefirestorekotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kevinhomorales.firebasefirestorekotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpView()
+    }
+
+    private fun setUpView() {
+        setUpActions()
+    }
+
+    private fun setUpActions() {
+
+    }
+
+}
